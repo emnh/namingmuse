bff4c9ebccbe7519e0386f61f63c6c8db898c339
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2004-10-09 23:24:16 +0000

    add pair of primitive tools for reading/submitting freedb records

---
+++ tools/freedb-dumprecord.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import os, re, sys
+from namingmuse.filepath import FilePath
+from namingmuse.albumtag import *
+from namingmuse.cddb import CDDBP
+
+if len(sys.argv) < 2:
+    sys.exit("usage: %s <album>" % sys.argv[0])
+    sys.exit(1)
+    
+dirname = sys.argv[1]
+
+fpath = FilePath(dirname)
+filelist = getfilelist(fpath)
+if len(filelist) == 0:
+    sys.exit("no files")
+albuminfo = getNmuseTag(filelist)
+if not albuminfo:
+    sys.exit("not tagged")
+
+cddb = CDDBP()
+cddb.encoding = sys.stdout.encoding
+albuminfo.setCDDBConnection(cddb)
+title = albuminfo.title
+
+#filename = title + ".cddb"
+filename = "%s %s.cddb" % (albuminfo.freedbdiscid, albuminfo.freedbgenre)
+fd = file(filename, "w")
+fd.write(albuminfo.freedbrecord)
+fd.close()
+print "Wrote " + filename
+print "This filename is needed when submitting with freedb-submit.py"
---
+++ tools/freedb-submit.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import os, re, sys
+
+if len(sys.argv) < 2:
+    print "usage: %s <recordfile>" % sys.argv[0]
+    sys.exit(1)
+
+# Read record
+filename = sys.argv[1]
+fd = file(filename)
+record = fd.read()
+fd.close()
+
+# Update revision
+newrecord = []
+lbreak = "\r\n"
+for line in record.splitlines():
+    if line.startswith('# Revision:'):
+        rev = int(line.split(':')[1]) + 1
+        line = '# Revision: %u' % rev
+    newrecord.append(line)
+newrecord = lbreak.join(newrecord)
+
+# Setup mail values
+address = 'freedb-submit@freedb.org'
+ident = os.path.splitext(filename)[0]
+if not re.search('^[a-z0-9]{8} [a-z]+$', ident):
+    sys.exit(ident + " is not a valid freedb `discid genre' pair")
+subject = "cddb %s" % ident
+
+# Save updated record
+fd = file(filename, "w")
+fd.write(newrecord)
+fd.close()
+
+# Send mail
+print "Subject:", subject
+cmd = 'cat "%s" | mutt -s "%s" %s' % (filename, subject, address)
+print "%", cmd
+os.system(cmd)
